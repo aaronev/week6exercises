c10ca83106bfc9545319457f37dbe4178768157e
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-06-11 20:44:41 -0700

    finishes the second part

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Part 1
 var testOne = "What kind of variable am I?"
 // This is a container carrying a string type
 var testTwo = 256
@@ -11,4 +12,35 @@ var testFive = '128'
 var testSix = ''
 //undefined
 
+//Part 2
+var testOne = "hello" + "there"
+// string => hellothere
+var testTwo = "100" + 28
+// string => 10028
+var testThree = true + 3
+// number => 4
+var testFour = 'false' + 5
+// string => false5
+var testFive = '128' - 3
+// numver => 125
+var testSix = '' * 9
+// number => 0
+var testSeven = 'zero' - 1
+// number => NaN
+var testEight = 'five'.length
+// number => 4
+var testNine = 'five'[0] + 'ever'
+// string => fever
+var testTen = 'a' > 'b'
+// boolean => false
+var testEleven = 'number'.length < 7
+// boolean => true
+var testTwelve = '12' == 12
+// boolean true
+var testThirteen = 'twelve' == 12
+//boolean false
+var testFourteen = '14' === 14
+//boolean false
+var testFifteen = 'Hi there'[0] + 'open sesame'[2] + 'alohamora'[1] + 'eulalia'[4] + 'harkonnen'[4] + 'BATMAN!'[6]
+//string => Hello!
 
