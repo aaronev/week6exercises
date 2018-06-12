8035262004b18f9b534e9becd8278778d818b495
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-06-11 20:32:08 -0700

    adds the first exercise

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var testOne = "What kind of variable am I?"
+// This is a container carrying a string type
+var testTwo = 256
+// This is a number
+var testThree = false
+//boolean
+var testFour = 'true'
+//string
+var testFive = '128'
+//string
+var testSix = ''
+//undefined
+
+
